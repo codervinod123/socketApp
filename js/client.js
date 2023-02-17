f36b7a3f2328e1b68deaad217761df11523a0f32
vinod <vinodpr737947@gmail.com>
2023-02-17 16:55:47 +0530

    server setup

--- js/client.js
+++ js/client.js
@@ -1,25 +1,32 @@
-  const socket=io('http://localhost:8000');
-
-  const form=document.getElementById('send-container');
+// const socket=io('http://localhost:3000');
+const socket=io("http://localhost:3000");
+  const form=document.getElementById('send_container');
   const messageInput=document.getElementById('messageInp')
   const messageContainer=document.querySelector(".container")
 
 
-   const append=(message,position)=>{
-   const messageElement=document.createElement('div')
+const append=(message,position)=>{
+     
+   const messageElement=document.createElement("div");
    messageElement.innerText=message;
-   messageElement.classList.add('message');
+   messageElement.classList.add("message");
    messageElement.classList.add(position);
    messageContainer.append(messageElement);
+
 }
 
-
-const naam= prompt("Enter your name to join the chat");
-socket.emit('new-user-joined', naam); 
-
- socket.on('user-joined', naam=>{
-append(`${naam} joined the chat`, 'right')
+form.addEventListener('submit',(e)=>{
+   e.preventDefault();
+   const message=messageInput.value;
+   append(`you ${message}`,'right');
+   socket.emit('send',message);
+   messageInput.value="";
 })
 
 
-console.log('vinod prajapti');
+const naam= prompt("Enter your name to join the chat","");
+
+socket.emit('new-user-joined',naam);
+socket.on('user-joined',naam =>{
+    append(`${naam} joined the chat`,'left');
+});
--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,41 +1,24 @@
-//nnode server which will handle socket .io connections
+// nnode server which will handle socket .io connections
 
 
-// const io=require('socket.io')(8000)
+const io=require('socket.io')(3000)
 
-// const users={};
+const users={};
 
-// io.on('connection',socket=>{
-//     socket.on('new-user-joined ',name=>{
-//          console.log(name);
-//          users[socket.id]=name;
-//          socket.broadcast.emit('user-joined', name)
-//     });
+// let cors = require("cors");
+// app.use(cors());
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+         console.log(name);
+         users[socket.id]=name;
+         socket.broadcast.emit('user-joined', name)
+    });
    
-// //      socket.on('s end', message=>{
-// //          socket.broadcast.emit('receive',{message: message,name: users[socket.id]})
-// //      });
-//    })
-// console.log('vinod prajapti mmmut');
+     socket.on('send', message=>{
+         socket.broadcast.emit('receive',{message: message,name: users[socket.id]})
+     });
 
+   
 
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-const val=(__dirname)
-console.log(val);
-app.get('/', (req, res) => {
-  res.sendFile(__dirname);
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+   })
